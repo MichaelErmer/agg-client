c99bc1d5569dcd7b40d255be4ab29854fe0781da
Michael Ermer <michael.ermer@ermer.de>
2016-12-17 09:50:39 +0100

    bump

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+'use strict'
+const net = require('net')
 
+function AggClient () {
+  let self = this
+}
+
+exports.createClient = function () {
+  return new AggClient(arguments)
+}
+
+require('./lib/commands')
